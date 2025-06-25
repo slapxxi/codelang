4d0bac6600ab2515ea23ebc7bae4a5de93d1fd90
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-06-25 10:19:32 +0300

    Add Tanstack Query

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,8 @@ import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration }
 import type { Route } from './+types/root';
 import './app.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
@@ -34,8 +36,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <Outlet />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {

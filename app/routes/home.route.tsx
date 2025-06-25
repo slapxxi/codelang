c8ffea541ac5f3b80997c590cdc14e77768e28ca
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-06-25 10:46:38 +0300

    Change route naming

---
+++ app/routes/home.route.tsx
@@ -0,0 +1,9 @@
+import type { Route } from './+types/home';
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'Codelang' }, { name: 'description', content: 'Codelang' }];
+}
+
+const HomeRoute = () => <div className="container mx-auto p-4 pt-16">Home</div>;
+
+export default HomeRoute;

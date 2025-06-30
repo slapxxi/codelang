f6b0ad2f39b4830d0466518b0688fc3de3525ec1
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-06-30 12:38:23 +0300

    Memo auth provider user value

--- src/app/providers/auth.provider.tsx
+++ src/app/providers/auth.provider.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useMemo } from 'react';
 import type { TUser } from '~/types';
 
 type AuthProviderProps = {
@@ -10,5 +10,7 @@ export const AuthContext = createContext<TUser | null>(null);
 
 export const AuthProvider: React.FC<AuthProviderProps> = (props) => {
   const { user, children } = props;
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+  const userMemo = useMemo(() => user, [user?.id]);
+
+  return <AuthContext.Provider value={userMemo}>{children}</AuthContext.Provider>;
 };
